a3c5b3cf8932843eb7042f07baf19de028550762
aj <anton23@gmail.com>
2014-08-18 14:44:53 -0700

    adding all

---
+++ javascripts/inject_toastmaster_email.js
@@ -0,0 +1,1 @@
+!function(){function t(t){return String.fromCharCode(t.charCodeAt(0)+1)}var e="mailto:",r="improv"+t("l")+"ast"+t("d")+"rs@gmail.co"+t("l"),o=document.getElementById("targetEmail");console.log(o.href),null!==o&&(o.href=e+r,o.innerHTML=r);var n=document.getElementById("forBrace");if(null!==o){var a="http://forms.brace.io/";n.setAttribute("action",a+r)}}();
