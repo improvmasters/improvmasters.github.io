8aaa3e0e057f8dfa9eea8a1057d8f80d65d9b97d
aj <anton23@gmail.com>
2014-08-20 09:58:31 -0700

    add analytics

---
+++ javascripts/inject_webmaster_email.js
@@ -0,0 +1,1 @@
+!function(){"use strict";function n(n){return String.fromCharCode(n.charCodeAt(0)+1)}var t="mailto:",e="an"+n("s")+"on23@gmail.co"+n("l"),o=document.getElementById("aj-email");null!==o&&(o.href=t+e)}();
